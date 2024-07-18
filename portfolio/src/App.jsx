eca3d84553b654dfc50bc76d624b385d993d9204
Ishanka Perera <pererawcpi.20@itfac.mrt.ac.lk>
2024-07-18 17:06:44 +0530

    Add About section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,16 +1,16 @@
-import styles from './App.module.css'
-import {Navbar} from './components/Navbar/Navbar'
-import { Profile } from './components/Profile/Profile';
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Profile } from "./components/Profile/Profile";
 
 function App() {
- 
-
   return (
     <div className={styles.App}>
       <Navbar />
       <Profile />
+      <About />
     </div>
-  )
+  );
 }
 
 export default App;
